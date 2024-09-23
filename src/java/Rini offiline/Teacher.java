c824998013941aea0116e6536994315677d65e6f
Sajid Inamdar <sajidinamdar90@gmail.com>
2024-09-23 21:34:59 +0530

    Teacher

---
+++ src/java/Rini offiline/Teacher.java
@@ -0,0 +1,9 @@
+public class Teacher {
+    public void teach()
+    {
+        System.out.println("Teacher teaches to studenst");
+    }
+    public void teaching(){
+        System.out.println("Teacher teaching to students");
+    }
+}
---
+++ src/java/Rini offiline/Teachermain.java
@@ -0,0 +1,7 @@
+public class Teachermain {
+    public static void main(String[] args){
+        Teacher t = new Teacher();
+        t.teach();
+        t.teaching();
+    }
+}
